b05bc6b92476376733bc7803afa56f87eba80387
Sanghyeon Lee <leesh6796@gmail.com>
2023-11-08 16:40:08 +0000

    glue task bug fix

--- exp.sh
+++ exp.sh
@@ -1,3 +1,3 @@
-./custom.sh copa > copa.txt
-./custom.sh hellaswag > hellaswag.txt
-./custom.sh squadv2 > squadv2.txt
+# ./custom.sh copa > copa.txt
+# ./custom.sh hellaswag > hellaswag.txt
+./custom.sh glue > glue.txt
